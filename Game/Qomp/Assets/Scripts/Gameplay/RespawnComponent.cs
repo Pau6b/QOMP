d9a5edb16e5b5a509b5f2c9add4d2e1411f59c32
Pau Murciano Julià <pau.murciano1@gmail.com>
2020-12-02 22:28:51 +0100

    Created snake

--- Game/Qomp/Assets/Scripts/Gameplay/RespawnComponent.cs
+++ Game/Qomp/Assets/Scripts/Gameplay/RespawnComponent.cs
@@ -8,7 +8,9 @@ namespace Game
     {
         public class RespawnComponent : MonoBehaviour
         {
-            // Start is called before the first frame update
+            public delegate void OnPlayerRespawned();
+            public event OnPlayerRespawned Respawned;
+
             [InspectorName("First Spawner")]
             public GameObject m_spawner;
             Vector3 m_velocity;
@@ -25,12 +27,14 @@ namespace Game
 
             private void OnTriggerEnter(Collider i_other)
             {
-                if (i_other.gameObject.tag == "Spike")
+                bool isSnakePartActive = i_other.gameObject.tag == "SnakePart" && i_other.GetComponent<SnakePartActivateComponent>().GetIsActive();
+                if (i_other.gameObject.tag == "Spike" || isSnakePartActive)
                 {
                     Vector3 newPosition = m_spawner.transform.position;
                     newPosition.y = transform.position.y;
                     transform.position = newPosition;
                     m_rb.velocity = m_velocity;
+                    Respawned?.Invoke();
                 }
                 if (i_other.gameObject.tag == "RespawnPoint" && m_spawner != i_other.gameObject)
                 {
