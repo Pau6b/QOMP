c6b13ba7f6a1ef1535ccae71547616e045c6cfaf
Pau Murciano Julià <pau.murciano1@gmail.com>
2020-12-07 23:59:18 +0100

    Changed key and added spawn point

--- Game/Qomp/Assets/Scripts/Gameplay/ActivateButton.cs
+++ Game/Qomp/Assets/Scripts/Gameplay/ActivateButton.cs
@@ -7,18 +7,29 @@ public class ActivateButton : MonoBehaviour
 
 
     // Start is called before the first frame update
-    private bool enab = true;
+    public delegate void ActivatedEvent(bool i_isActivated);
+    public ActivatedEvent OnActivatedEvent;
 
-    public bool GetIsEnabled()
-    {
-        return enab;
-    }
+    [SerializeField] List<GameObject> m_objectsToSwap;
+    [SerializeField] bool isKey = true;
+    private bool m_isActive = false;
 
-    private void OnCollisionEnter(Collision collision)
+
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            enab = false;
+            m_isActive = !m_isActive;
+            OnActivatedEvent?.Invoke(m_isActive);
+            foreach(GameObject objectToSwap in m_objectsToSwap)
+            {
+                bool objectIsActive = objectToSwap.activeInHierarchy;
+                objectToSwap.SetActive(objectIsActive);
+            }
+            if (isKey)
+            {
+                Destroy(this.gameObject);
+            }
         }
     }
 
