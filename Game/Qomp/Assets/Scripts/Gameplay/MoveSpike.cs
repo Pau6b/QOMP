3e9f4398d9ec2d4b03bcf684dab40b076b1a4e6b
Pau Murciano Julià <pau.murciano1@gmail.com>
2020-12-08 19:15:21 +0100

    Added things in medieval level

---
+++ Game/Qomp/Assets/Scripts/Gameplay/MoveSpike.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Game
+{
+    namespace Gameplay
+    {
+        public class MoveSpike : MonoBehaviour
+        {
+
+            [SerializeField] private Vector3 m_direction;
+            [SerializeField] private float m_speed;
+            private Rigidbody m_rb;
+
+            // Start is called before the first frame update
+            void Start()
+            {
+                m_rb = GetComponent<Rigidbody>();
+                m_rb.velocity = m_direction.normalized * m_speed;
+            }
+
+            private void OnTriggerEnter(Collider other)
+            {
+                m_rb.velocity = -m_rb.velocity;
+            }
+        }
+
+    }
+}
