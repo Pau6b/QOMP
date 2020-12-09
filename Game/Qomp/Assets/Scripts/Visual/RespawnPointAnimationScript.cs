42e57a0b67335b80ef4e63ed4acb22f9e20e0eed
Pau Murciano Julià <pau.murciano1@gmail.com>
2020-12-09 18:41:37 +0100

    AnimatedSpawnPoint

---
+++ Game/Qomp/Assets/Scripts/Visual/RespawnPointAnimationScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Game
+{
+    namespace Visual
+    {
+
+        public class RespawnPointAnimationScript : MonoBehaviour
+        {
+            // Start is called before the first frame update
+            private Animator m_animator;
+            void Start()
+            {
+                m_animator = GetComponent<Animator>();
+                m_animator.SetBool("isActive", false);
+            }
+
+            public void SetActive(bool i_isActive)
+            {
+                m_animator.SetBool("isActive", i_isActive);
+            }
+
+            public void SetFirstAsSpawnerActive()
+            {
+                m_animator.SetBool("isActive", true);
+                m_animator.playbackTime = 0.3f;
+            }
+        }
+    }
+}
