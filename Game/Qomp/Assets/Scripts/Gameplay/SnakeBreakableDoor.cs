63c78c50a9334a1c70bc33591454a043eeb74192
Pau Murciano Julià <pau.murciano1@gmail.com>
2020-12-09 17:18:40 +0100

    SnakeBreakableDoor

---
+++ Game/Qomp/Assets/Scripts/Gameplay/SnakeBreakableDoor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    namespace Gameplay
+    {
+        public class SnakeBreakableDoor : MonoBehaviour
+        {
+            [SerializeField] private SnakeComponent m_snakeComponent;
+            private Collider m_collider;
+
+            void Start()
+            {
+                m_collider = GetComponent<Collider>();
+                m_snakeComponent.SnakeStarted += OnSnakeStart;
+                m_snakeComponent.SnakeEnded += OnSnakeEnd;
+                m_collider.isTrigger = false;
+            }
+
+            private void OnDestroy()
+            {
+                m_snakeComponent.SnakeStarted -= OnSnakeStart;
+                m_snakeComponent.SnakeEnded -= OnSnakeEnd;
+            }
+
+            private void OnTriggerEnter(Collider other)
+            {
+                if (other.tag == "Player")
+                {
+                    m_snakeComponent.OnDoorBroken();
+                    GameObject.Destroy(transform.gameObject);
+                }
+            }
+
+            void OnSnakeStart()
+            {
+                m_collider.isTrigger = true;
+            }
+
+            void OnSnakeEnd(SnakeEndReason i_endReason)
+            {
+                m_collider.isTrigger = false;
+            }
+        }
+
+    }
+}
