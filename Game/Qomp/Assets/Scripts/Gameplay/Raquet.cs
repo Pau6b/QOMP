e68a32c80d7caa43aa9c1fc48b1f6c763e7bcd9a
Pau Murciano Julià <pau.murciano1@gmail.com>
2020-12-12 19:03:32 +0100

    Fixed rackets

--- Game/Qomp/Assets/Scripts/Gameplay/Raquet.cs
+++ Game/Qomp/Assets/Scripts/Gameplay/Raquet.cs
@@ -17,12 +17,19 @@ public class Raquet : MonoBehaviour
     [SerializeField] private Vector3 m_automaticDir;
     [SerializeField] private float m_automaticVelocity;
     bool m_isFollowingPlayer = false;
+    bool m_isChangingDirection = false;
+    int numberOfFramesWithTheSamePos = 0;
+    Vector3 m_lastPosition;
+    Vector3 m_firstAutomaticDirection;
 
     void Start()
     {
+        m_lastPosition = transform.position;
         m_tireRb = GetComponent<Rigidbody>();
         Vector3 vel = new Vector3(1, 0, 1);
         vel.Normalize();
+        m_automaticDir.Normalize();
+        m_firstAutomaticDirection = m_automaticDir;
         m_tireRb.velocity = new Vector3(vel.x*m_direction.x, 0, vel.z*m_direction.z);
     }
 
@@ -64,42 +71,40 @@ public class Raquet : MonoBehaviour
             speed = new Vector3(speed.x * m_direction.x, 0, speed.z * m_direction.z);
             m_tireRb.velocity = speed;
         }
-        
         else if (m_automatic)
         {
             if (m_isFollowingPlayer)
             {
-                m_automaticDir = m_tireRb.velocity.normalized;
+                m_automaticDir = m_tireRb.velocity;
+                m_automaticDir.y = 0;
+                m_automaticDir.Normalize();
+                if (m_automaticDir == new Vector3(0,0,0))
+                {
+                    m_automaticDir = m_firstAutomaticDirection;
+                }
                 m_isFollowingPlayer = false;
             }
-            m_tireRb.velocity = new Vector3(m_direction.x*m_automaticDir.x, m_direction.y * m_automaticDir.y, m_direction.z * m_automaticDir.z);
+            if (m_lastPosition == transform.position)
+            {
+                numberOfFramesWithTheSamePos++;
+                if (numberOfFramesWithTheSamePos >= 5)
+                {
+                    numberOfFramesWithTheSamePos = 0;
+                    m_automaticDir = -m_automaticDir;
+                }
+            }
+            else
+            {
+                m_lastPosition = transform.position;
+                numberOfFramesWithTheSamePos = 0;
+            }
+            m_isChangingDirection = false;
+            m_tireRb.velocity = new Vector3(m_direction.x*m_automaticDir.x, m_direction.y * m_automaticDir.y, m_direction.z * m_automaticDir.z)*m_automaticVelocity;
         }
-
         else
         {
             m_tireRb.velocity = new Vector3(0,0,0);
         }
-
-        /*Vector2 offset = new Vector2(this.transform.localScale.x, this.transform.localScale.z);
-        float velUfo_z = m_Ufo.GetComponent<Rigidbody>().velocity.z;
-        float velUfo_x = m_Ufo.GetComponent<Rigidbody>().velocity.z;
-        Vector2 pos_Ufo = new Vector2(m_Ufo.transform.position.x, m_Ufo.transform.position.z);
-        Vector2 pos_Raq = new Vector2(this.transform.position.x, transform.position.z);
-
-        if (m_direction.z != 0 && ((m_direction.z > 0 && velUfo_z < 0) || (m_direction.z < 0 && velUfo_z > 0)) && 
-            ((pos_Ufo.y < pos_Raq.y + offset.y/2) && (pos_Ufo.y > pos_Raq.y - offset.y/2)) )
-        {
-            //m_direction.z = -m_direction.z;
-            //m_tireRb.velocity = m_speed * m_direction;
-            m_speed -= m_speed;
-        }
-        else if (m_direction.x != 0 && ((m_direction.x > 0 && velUfo_x < 0) || (m_direction.x < 0 && velUfo_x > 0)) &&
-            ((pos_Ufo.x < pos_Raq.x + offset.x/2) && (pos_Ufo.x > pos_Raq.x - offset.x/2)) )
-        {
-            //m_direction.x = -m_direction.x;
-            //m_tireRb.velocity = m_speed * m_direction;
-            m_speed -= m_speed;
-        }*/
 
     }
 
@@ -107,11 +112,14 @@ public class Raquet : MonoBehaviour
     {
         if (collision.gameObject.tag == "Wall")
         {
-            if (!m_isFollowingPlayer)
+            if (!m_isFollowingPlayer && !m_isChangingDirection)
             {
+                m_isChangingDirection = true;
                 m_automaticDir = -m_automaticDir;
+                m_tireRb.velocity = new Vector3(m_direction.x * m_automaticDir.x, m_direction.y * m_automaticDir.y, m_direction.z * m_automaticDir.z) * m_automaticVelocity;
             }
         }
     }
+
 
 }
